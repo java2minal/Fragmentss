0058196fbfa4a89aad3c749e8c82185cee9b5e67
java2minal <minal_saeed@hotmail.com>
2015-05-17 21:00:40 +0800

    Initial commit

---
+++ app/src/main/java/com/luct/minal/fragmentss/MainActivity.java
@@ -0,0 +1,36 @@
+package com.luct.minal.fragmentss;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selectFrag(View view) {
+        Fragment fr;
+
+        if(view == findViewById(R.id.button2)) {
+            fr = new fragmentTwo();
+
+        }else {
+            fr = new fragmentOne();
+        }
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+
+    }
+
+}
